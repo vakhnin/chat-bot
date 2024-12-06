77634bb7974c99edc2e495466577398900a378c7
Sergey Vakhnin <sergey.vakhnin@gmail.com>
2024-12-06 09:26:53 +0300

    Вывод нескольких котировок

--- currency_rates.py
+++ currency_rates.py
@@ -2,6 +2,8 @@ import os
 
 import requests
 
+currencies = ["USD", "EUR", "GBP"]
+
 
 def get_currency_rates(api_key: str) -> dict:
     url = f"https://openexchangerates.org/api/latest.json?app_id={api_key}"
@@ -21,6 +23,14 @@ def format_currency_data(currency_name: str, rate: float) -> str:
     return f"Курс {currency_name}: {rate:.2f}"
 
 
+def process_multiple_currencies(data: dict, currencies: list) -> dict:
+    rates = {}
+    for currency in currencies:
+        if currency in data['rates']:
+            rates[currency] = data['rates'][currency]
+    return rates
+
+
 if __name__ == '__main__':
     currency_name = "USD/RUB"
     api_key = os.getenv("API_KEY")
@@ -30,3 +40,6 @@ if __name__ == '__main__':
     usd_to_rub = process_currency_data(currency_data)
     formatted_data = format_currency_data(currency_name, usd_to_rub)
     print(formatted_data)
+    rates = process_multiple_currencies(currency_data, currencies)
+    for currency, rate in rates.items():
+        print(f"Курс {currency}: {rate}")
